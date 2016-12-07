5aaa9627c79ab30154ba5a62cf4903af6fbae817
Brian Bernheim <btbernheim@gmail.com>
2016-12-07 01:35:15 -0800

    Add questions with puts, create a variable for each question to equal input.

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name1 = gets.chomp
+
+puts "How old are you? What year were you born?"
+age = gets.chomp
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance? (y/n)"
+insurance = gets.chomp
