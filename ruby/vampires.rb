dd892c779295426ecc1eb0f7a0c329156aa0aad1
Brian Bernheim <btbernheim@gmail.com>
2016-12-09 01:01:12 -0800

    Add message asking for allergies, if user input = sunlight, put definitely a vampire, if user input = done, end loop.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,6 +3,7 @@ employee = gets.to_i
 
 vampire = 0
 
+#Loop until employee processing is met.
 until vampire == employee 
 
 	puts "What is your name?"
@@ -14,7 +15,7 @@ until vampire == employee
     		else name2 = true
     	end
   
-  puts "How old are you? What year were you born?"
+    puts "How old are you? What year were you born?"
 	age = gets.to_i
 	birth_year = gets.to_i
   		if birth_year + age == 2016
@@ -22,7 +23,7 @@ until vampire == employee
   			else age2 = false
   		end
 		
-		puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 	garlic = gets.chomp
     	if garlic == "y"
     		garlic2 = true
@@ -31,20 +32,35 @@ until vampire == employee
     	end
 	
 	puts "Would you like to enroll in the company’s health insurance? (y/n)"
-	insurance = gets.chomp
-      	if insurance == "y"
-      		insurance2 = true
-      		else insurance == "n"
-      			insurance2 = false
-      	end
-
+  	insurance = gets.chomp
+        if insurance == "y"
+        	insurance2 = true
+        	else insurance == "n"
+       			insurance2 = false
+       	end
+      	
+    puts "List any allergies you might have (Enter 'done' when finsihed):"
+    allergies = gets.chomp
+	    until allergies.include? "sunlight" or allergies.include? "done"
+	      allergies = gets.chomp
+	    end
+	    if allergies.include? "sunlight"
+	    	allergies2 = true
+			else
+		      allergies2 = false
+	    end
+  
 	if !name2
-    puts "Definitely a vampire."
-    elsif age2 && (garlic2 || insurance2)
-    	puts "Probably not a vampire"
-    elsif !age2 && (!garlic2 || !insurance2)
-      puts "Probably a vampire."
-    else puts "Results inconclusive"
-  end
+    	puts "Definitely a vampire."
+	    elsif allergies2
+	    	puts "Probably a vampire."
+	    elsif age2 && (garlic2 || insurance2)
+	    	puts "Probably not a vampire"
+	    elsif !age2 && (!garlic2 || !insurance2)
+	    	puts "Probably a vampire."
+	    else puts "Results inconclusive"
+	  end
   vampire += 1
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
