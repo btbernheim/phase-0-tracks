6bdba5a5374b4e89c1aae836b3a42fe4f295e00d
Brian Bernheim <btbernheim@gmail.com>
2016-12-14 17:02:36 -0800

    Alter code to remove if statement block to modify key pair value. Add 1 if block the takes a key, asks for a value, and resigns the key to that balue in the hash.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -56,65 +56,18 @@ client_details[:bedroomkey] = bedrooms
 p client_details
 
 
-#Allow for keys to be updated
 puts "Would you like to update any of your information? "
 answer = gets.chomp
 if answer == "yes"
 	puts "Which section would you like to alter?"
-	puts "[Enter name, age, children, decor, color, house, pets, or bedrooms]: "
-	section = gets.chomp
-		if section == "name"
-			puts "Enter you name: "
-			new_name = gets.chomp
-			client_details[:namekey] = new_name
-			elsif section == "age"
-				puts "Enter you age: "
-				new_age = gets.to_i
-				client_details[:agekey] = new_age
-				p client_details
-			elsif section == "children"
-				puts "How many children do you have?"
-				new_children = gets.to_i
-				client_details[:childrenkey] = new_children
-				p client_details
-			elsif section == "decor"
-				puts "What is your preferred decor theme?"
-				new_decor = gets.chomp
-				client_details[:decorkey] = new_decor
-				p client_details
-			elsif section == "color"
-				puts "You like bright colors. (T/F)?"
-				new_color = gets.chomp
-				client_details[:colorkey] = new_color
-				p client_details
-			elsif section == "home"
-				puts "What kind of house do you live in: "
-				new_house = gets.chomp
-				client_details[:homekey] = new_house
-				p client_details
-			elsif section == "pets"
-				puts "You want your homes decor to be pet friendly. (T/F)?"
-				new_pet = gets.chomp
-				client_details[:petskey] = new_pet
-				p client_details
-			elsif section == "bedrooms"
-				puts "How many bedrooms is your home?"
-				new_bedrooms = gets.to_i
-				client_details[:bedroomkey] = new_bedrooms
-				p client_details
-			else p client_details
-		end
+	section = gets.chomp.to_sym
+	puts "Enter your new data:"
+	new_data = gets.chomp.to_sym
+	client_details[section] = new_data
+	p client_details
 	else answer == "none"
-		p client_details
+    p client_details
 end
-
-p client_details
-	
-
-
-#Print the final reslts of the hash
-
-
 
 
 
