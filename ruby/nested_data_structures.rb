715de75466750ca888341674a80fcffd8aa1b0de
Brian Bernheim <btbernheim@gmail.com>
2016-12-15 22:20:25 -0800

    Add peudocode to describe the nested data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,8 +1,8 @@
 #Design a nested data structure using a hotel 
 
 #Create the apartment complex using a hotel:
-
 hotel = {
+#Add three floors that describe how many rooms are available, room type, and the contents within them.
 	first_floor: {
 		room_quantity: 10,
 		room_type: {
@@ -16,6 +16,8 @@ hotel = {
 				'bathroom'
 			]
 		}
+#Creates a first floor hash that holds room_quantity and room_type.
+#Gives the roomtype hash an array of room contents.
 	},
 	second_floor: {
 		room_quantity: 7,
@@ -40,6 +42,7 @@ hotel = {
 				'chair',
 				'bathroom'
 			]
+#Adds a second hash that holds an array within the room_type hash.
 		}
 	},
 	third_floor: {
@@ -80,6 +83,7 @@ hotel = {
 	}
 }
 
+#Add statements that print out a nested piece of data
 puts hotel[:first_floor][:room_quantity]
 puts hotel[:first_floor][:room_type][:terrace_studio][0]
 puts hotel[:second_floor][:room_quantity]
@@ -89,4 +93,5 @@ puts hotel[:third_floor][:room_quantity]
 puts hotel[:third_floor][:room_type][:executive_suite][8]
 puts hotel[:third_floor][:room_type][:penthouse][13]
 hotel[:first_floor][:room_type][:terrace_studio].push("mini fridge")
+#Appends the terrace studio array with 'mini fridge' at index 7
 puts hotel[:first_floor][:room_type][:terrace_studio][7]
