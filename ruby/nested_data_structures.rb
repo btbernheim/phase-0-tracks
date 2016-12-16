a00d89c01265790a5ec65080f64f294775c55067
Brian Bernheim <btbernheim@gmail.com>
2016-12-15 20:12:37 -0800

    Initial commit. Add a hash titled 'hotel' to create a nested data structure. Add four floors, each with their own hash to include a type of room hash, containing a nested array of items the rooms contain. Make 'bathroom', within the array, its own hash with an array value.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,58 @@
+#Design a nested data structure using a hotel 
+
+#Create the apartment complex using a hotel:
+
+apartment_complex = {
+	first_floor: {
+		number_of_rooms: 10,
+		type_of_rooms: {
+			terrace_studio: [
+				'queen bed', 
+				'tv',
+				'night stnad'
+				'alarm clock'
+				'dresser'
+				'desk'
+				'chair'
+				bathroom: ["toilet", "mirror", "shower", "sink",]
+			]
+
+		}
+	}
+
+	second_floor: {
+		number_of_rooms: 7,
+		type_of_rooms: {
+			bungalow_1_bed: [
+					'queen bed', 
+					'tv',
+					'night stnad',
+					'alarm clock',
+					'dresser',
+					'desk',
+					'chair',
+					bathroom: ["toilet", "mirror", "shower", "sink",]
+			]
+				bungalow_2_bed: [
+					'2 queen beds', 
+					'tv',
+					'night stnad',
+					'alarm clock',
+					'dresser',
+					'desk',
+					'chair',
+					bathroom: ["toilet", "mirror", "shower", "sink",]
+			]
+		}
+	}
+
+	
+	fourth_floor: = third_floor:
+	
+	penthouse_suites: {
+		number_of_rooms: 2
+
+	}
+
+
+}
