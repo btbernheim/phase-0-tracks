2e7e8287d51eba7cb6bd467f56de44f75ef6663b
Brian Bernheim <btbernheim@gmail.com>
2016-12-17 12:47:54 -0800

    Create method that iterates through an array  to match a given integer witht he array's index values

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,48 @@
+#Release 0
+#Write a method that iterates trhough an array (.each)
+#Look to check if the value is the same the given value.
+#Return the index of the item if it exists within the array.
+#Return nil if it's not in the array.
+array = [1, 2, 3, 4, 5]
+int = 7
+def int_search(array, int)
+	found = nil
+	array.each_index do |char|
+		if array[char] == int
+			p char
+			found = true
+		end
+	end 
+	if found == nil
+		p found
+	end
+end
+
+int_search(array, int)
+		#for each index, find the value of that index and comapre it to the parameter
+
+
+
+
+#Release 1
+#Create a method that will recieve the number of the Fibonacci sequence.
+#Starting with numbers 0 and 1, adding the two numbers
+#Create an array with a length of two
+#Add two values to the array.
+#Appened (push) a value at the end of the array, remove the value at the front of the array (.shift)
+#Create another array containg all the values.
+#Repeat line 10-12 relative to the value recieved at line 8.
+#Return the array at line 13
+
+#Release 2
+#Receive an array
+#Iterate through the array
+#Bubble sort: "repeatedly steps through the list to be sorted, 
+#compares each pair of adjacent items and swaps them if they are in the wrong order.
+#The pass through the list is repeated until no swaps are needed, 
+#which indicates that the list is sorted. 
+#Think through bubble sort.
+#Try to loop throughout the array until num_swap == 0
+#Compare values within an array and swap their index if one value is smaller at the compared index location.
+#Record the nummer of swaps = num_swap
+#Assign a value of 1, start looping, 
