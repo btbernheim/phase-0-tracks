d52136c23f3ad427a886f9e9112cd9fcb8099f3b
Brian Bernheim <btbernheim@gmail.com>
2016-12-18 17:58:25 -0800

    djj

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,11 +1,13 @@
 #Release 0
-#Write a method that iterates trhough an array (.each)
-#Look to check if the value is the same the given value.
+#Write a method that iterates through an array (.each)
+#Check if any values in the array are equal to the given value.
 #Return the index of the item if it exists within the array.
 #Return nil if it's not in the array.
-#for each index, find the value of that index and comapre it to the parameter
+#(In sum: For each index, find the value of that index and comapre it to the parameter.)
+
 array = [1, 2, 3, 4, 5]
 int = 7
+
 def int_search(array, int)
 	found = nil
 	array.each_index do |char|
@@ -22,14 +24,16 @@ end
 int_search(array, int)
 
 #Release 1
-#Create a method that will recieve the number of the Fibonacci sequence.
-#Starting with numbers 0 and 1, adding the two numbers
+#Create a method that will recieve a number that specifies how long the Fibonacci sequence will be.
+#Start with the numbers 0 and 1, add the two numbers to begin the sequence
 #Create an array with a length of two
 #Add two values to the array.
-#Appened (push) a value at the end of the array, remove the value at the front of the array (.shift)
+#Push the sum of the past two values to the end of the array, remove the value at the front of the array (.shift)
 #Create another array containg all the values.
-#Repeat line 10-12 relative to the value recieved at line 8.
-#Return the array at line 13
+#Repeat the process of adding the two values together and appending the second array with the reult
+#Do this until the arrays length = the user provided integer - 2
+#Return the result
+
 puts "Enter an integer:"
 fib_int = gets.to_i
 
