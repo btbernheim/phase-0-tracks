1700f2031b05020b2ea63c5a4c38b61620ef71bb
Brian Bernheim <btbernheim@gmail.com>
2016-12-21 00:03:29 -0800

    Create a method that adds an item to the list. Create a method that deletes an item from the list.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,25 +1,49 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps: 
-  # [fill in any steps here]
+  # 
   # set default quantity
   # print the list to the console [can you use one of your other methods here?]
 # output: [what data type goes here, array or hash?]
+
+list_of_items = "soda avocado banana crackers bread"
+
+def create_list(list_of_items)
+grocery_list = {}
+list_of_items.split(' ').each do |item|
+  grocery_list[item] = 1
+  end
+return grocery_list
+end
+
+grocery_list = create_list(list_of_items)
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps:
 # output:
 
+def add_item(groceries_hash, item_name, quantity)
+  hash_of_groceries[item_name] = quantity
+end
+
+add_item(grocery_list, "apple", 15)
 # Method to remove an item from the list
 # input:
 # steps:
 # output:
 
+def remove_item(groceries_hash, item_name)
+  groceries_hash.delete(item_name)
+end
+
+remove_item(grocery_list, "soda")
+
 # Method to update the quantity of an item
 # input:
 # steps:
 # output:
+
 
 # Method to print a list and make it look pretty
 # input:
