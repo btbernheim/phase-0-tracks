86e6e5a438388f784509352ceabed7314f8d765b
Brian Bernheim <btbernheim@gmail.com>
2016-12-22 22:02:35 -0800

    Add methods to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,24 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def fetch(toy)
+	    puts "I brought back the #{toy}!"
+	    toy
+	end
 
+	def roll_over(int)
+	  	count = 0
+	  	until count == int
+	  		p "Woof!"
+	  		count += 1
+	  	end
+  	end
+
+ 	def roll_over
+  		p "*rolls over*"
+  	end
+
+	def dog_years(human_yrs)
+	  	conversion = human_yrs * 7
+	  	puts "New integer in dog years: #{conversion}"
+	end
 end
-
