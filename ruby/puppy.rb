8c8f7a36bb4f17362c611a97958a2ad1be9e4e28
Brian Bernheim <btbernheim@gmail.com>
2016-12-22 20:55:48 -0800

    Describe the characteristics and behavaior of a puppy.

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+#Species:
+#Canine 
+
+#Breed: 
+#Golden Retriever
+
+#Characteristics
+#Fur color: feathered gold
+#Fur length: long
+#Leg count: 4
+#Tail length: long
+#Size: medium-large
+#age: 1
+
+#Behavior:
+#Athletic
+#Family friendly
+#Energetic
+#Eager to please
+#Cheerful
