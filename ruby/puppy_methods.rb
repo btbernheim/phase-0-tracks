078b670c3563ce29c65a4d33172263b6ea111401
Brian Bernheim <btbernheim@gmail.com>
2016-12-22 23:22:36 -0800

    Add initialize method, add driver code to test instances of puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,5 @@
+#Release 1:
+
 class Puppy
 
 	def fetch(toy)
@@ -5,7 +7,7 @@ class Puppy
 	    toy
 	end
 
-	def roll_over(int)
+	def speak(int)
 	  	count = 0
 	  	until count == int
 	  		p "Woof!"
@@ -21,4 +23,53 @@ class Puppy
 	  	conversion = human_yrs * 7
 	  	puts "New integer in dog years: #{conversion}"
 	end
+
+	def tail_wag(wag_num)
+		count = 0
+		until count == wag_num
+			p "Wag."
+			count += 1
+		end
+	end
+
+	def initialize
+		puts "Initializing new puppy instance ..."
+	end
+	#The initialize method will run at the beginnning of the program, without altering driver code.
+	#Misspelling initialize will result in it not printing, ruby won't recognize it's special status.
+
 end
+
+#Initialize an instance of puppy
+golden_pup = Puppy.new
+#Driver code:
+golden_pup.fetch('ball')
+golden_pup.speak(5)
+golden_pup.roll_over
+golden_pup.dog_years(5)
+golden_pup.tail_wag(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
