9104e85c2d5cdd0d42926c645fa73fbb9c5e3bb2
Brian Bernheim <btbernheim@gmail.com>
2016-12-28 04:13:30 -0800

    Edit pseudo code. Move reindeer_ranking into get_mad_at instance method, change to instance mehtod.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,4 @@
+#Create a Santa class
 class Santa
 	attr_reader :age, :ethnicity
 	attr_accessor :gender
@@ -6,8 +7,6 @@ class Santa
 		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
-		reindeer_ranking = 
-			["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = rand(140)
 	end
 
@@ -15,12 +14,10 @@ class Santa
 	#def age
 		#@age
 	#end
-
 	#Getter method
 	#def ethnicity
 		#@ethnicity
 	#end
-
 	#Setter method
 	#def gender = (new_gender)
 	#	@gender = new_gender
@@ -30,32 +27,33 @@ class Santa
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
-
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!" 
 	end
-
 
 	def celebrate_birthday
 		@age += 1
 	end
 
-
 	def get_mad_at(reindeer_name)
+		@reindeer_ranking = 
+			["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@reindeer_ranking.delete(reindeer_name)
 		@reindeer_ranking.insert(8, reindeer_name)
 	end
 end
 
+#Driver code
 santa1 = Santa.new("male", "American")
 santa1.celebrate_birthday
 puts "This santa is a #{santa1.age} year old #{santa1.ethnicity} #{santa1.gender}"
 santa1.speak
 santa1.eat_milk_and_cookies("choclate chip cookie")
-santa1.get_mad_at("Rudolph")
+p santa1.get_mad_at("Rudolph")
 santa1.gender = "female"
 puts "This santa is a #{santa1.age} year old #{santa1.ethnicity} #{santa1.gender}"
 
+#Create 25 santa instances of Santa by pick a random index from the gender and ethinicity arrays 
 gender_arr = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 ethnicity_arr = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
