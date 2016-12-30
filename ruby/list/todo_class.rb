bfcf1119783be6daa65e92555dffdcaa638a89d6
Brian Bernheim <btbernheim@gmail.com>
2016-12-30 05:43:58 -0800

    Alter code for each instance method so that the rspec test passes for each method.

--- ruby/list/todo_class.rb
+++ ruby/list/todo_class.rb
@@ -1,22 +1,24 @@
 class TodoList
-	def initialize(list_array)
+
+	def initialize(list_item)
 		puts "Initializing todo list..."
-		@list_array = list_array
+		@list_item = list_item
 	end
 
-	def store_list
-		@list_array
+	def get_items
+		@list_item
 	end
 
 	def add_item(item_add)
-		@todo_list << item_add
+		@list_item << item_add
 	end
 
-	def delete_items(items_del)
-		@list_array.delete(items_del)
+	def delete_item(items_del)
+		@list_item.delete(items_del)
 	end
 
-	def retrieve_item(item_ret)
-		@list_array.find_at(item_ret)
+	def get_item(item_ret)
+		@list_item.fetch(item_ret)
 	end
 end
+
