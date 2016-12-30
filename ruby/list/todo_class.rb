7d202c366fc991f989e421d2198f6654d42ca4fa
Brian Bernheim <btbernheim@gmail.com>
2016-12-30 04:40:07 -0800

    Create class outline to be rspec tested.

---
+++ ruby/list/todo_class.rb
@@ -0,0 +1,15 @@
+class TodoList
+	def initialize(item)
+		puts "Initializing a todo list."
+		@item = item
+	end
+
+	def add_item(items_add)
+	end
+
+	def delete_items(items_del)
+	end
+
+	def retrieve_item(item_ret)
+	end
+end
