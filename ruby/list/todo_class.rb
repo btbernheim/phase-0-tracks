3931ef52187dd3f1c5c3dced98129799625f193d
Brian Bernheim <btbernheim@gmail.com>
2016-12-30 05:11:24 -0800

    Add a method to store an array. Add code to each method to complete their operation.

--- ruby/list/todo_class.rb
+++ ruby/list/todo_class.rb
@@ -1,15 +1,22 @@
 class TodoList
-	def initialize(item)
-		puts "Initializing a todo list."
-		@item = item
+	def initialize(list_array)
+		puts "Initializing todo list..."
+		@list_array = list_array
 	end
 
-	def add_item(items_add)
+	def store_list
+		@list_array
+	end
+
+	def add_item(item_add)
+		@todo_list << item_add
 	end
 
 	def delete_items(items_del)
+		@list_array.delete(items_del)
 	end
 
 	def retrieve_item(item_ret)
+		@list_array.find_at(item_ret)
 	end
 end
