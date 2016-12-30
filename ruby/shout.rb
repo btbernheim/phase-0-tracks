b92d41ccc121b76dea1a672f80cc8bdd1fb29ff8
Brian Bernheim <btbernheim@gmail.com>
2016-12-29 23:34:06 -0800

    Add two methods to the module. Add driver code testing the methods.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,14 @@
 #Define the module
 module Shout
 
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yelling_happily
+  	puts "I'm yelling, but I'm happy."
+  end
 end
+
+Shout.yell_angrily("I don't understand self")
+Shout.yelling_happily("I don't understand self, but I want to")
