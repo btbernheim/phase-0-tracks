01249fe86c66bb411b5804b5e4a05ff472dbeb12
Brian Bernheim <btbernheim@gmail.com>
2017-01-03 05:19:59 -0800

    Add first test.

--- ruby/game_project/guessing_game_spec.rb
+++ ruby/game_project/guessing_game_spec.rb
@@ -1,11 +1,10 @@
 require_relative 'guessing_game'
 
 describe GuessingGame do
-	let(:game) { GuessingGame.new(player_input)}
+	let(:game) { GuessingGame.new("hello")}
 
-	it do
-	end
-
-	it do
+	it "checks if a guessed letter is in target word, updates word if it is" do
+		game.guess_the_letter("h")
+		expect(game.display_word).to eq "h____"
 	end
 end
