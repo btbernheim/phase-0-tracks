49414a3d15c0892670c8f7f17d076c7219888486
Brian Bernheim <btbernheim@gmail.com>
2017-01-07 01:52:03 -0800

    Fix code by adding necessary 'end'

--- ruby/game_project/guessing_game.rb
+++ ruby/game_project/guessing_game.rb
@@ -13,11 +13,12 @@ class GuessingGame
 		g_letter.downcase
 		if !@word.include? (g_letter)
 			    puts "Nope..."
+		end
 		@word.split("").each_with_index do |w_letter, index|
 			if g_letter == w_letter
 				@display_word[index] = g_letter
 				puts "Player 2, here is your progress towards guessing the word:"
-        		p @display_word
+        p @display_word
 			end
 		end
 	end
@@ -27,6 +28,7 @@ class GuessingGame
 		if @word == @display_word
 	  		puts "Congratulations Player 2, you won!"
 	  		true 
+	  	end
 	end
 	
 	# Determine losing conditions.
@@ -34,6 +36,7 @@ class GuessingGame
 		if @attempts == @word.length + 2
 		    puts "Player 1 won, player 2, why are you so bad guessing?"
 		    true
+		  end
 	end
 	
 	# Set the conditions for the interface's loop to stop.
@@ -53,7 +56,7 @@ guessed_letters = []
 
 #Create an interface for the users to play the game.
 until game.game_is_over
-  	puts "Player 2, please guess a letter..."
+  puts "Player 2, please guess a letter..."
 	letter_guess = gets.chomp
 	if guessed_letters.include? letter_guess
 		puts "You already tried that letter, enter a new one."
