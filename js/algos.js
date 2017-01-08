5f9b0f205262c4674b9b9cdacdfa2d9c1a686148
Brian Bernheim <btbernheim@gmail.com>
2017-01-07 19:33:57 -0800

    Add driver code that generates an array, prints the array, feeds the array to the stringDetector function, and prints the result 10 times.

--- js/algos.js
+++ js/algos.js
@@ -1,11 +1,12 @@
+// Release 0
 // Initialize a function that takes an array of strings as an argument, 
 // The output of the function will be the longest string of the array
 // Sort through the array by comparing each index to the length of the logest word up to that index.
 // Print the longest string.
-// var stringArr = ["hi", "bye", "Hello world", "bri" ];
 
 function stringDetector(stringArr) {
-	var longestLength, longestWord; // You don't have to pre set a variable before hand in JS, just declare it.
+	var longestLength = 0;
+	var longestWord = "";
 	for (var i = 0; i < stringArr.length; i++) {
 		if (stringArr[i].length > longestLength) {
 			longestLength = stringArr[i].length;
@@ -13,36 +14,33 @@ function stringDetector(stringArr) {
 		}
 	}
 	return longestWord
-	//console.log(longestWord);
 }
 
-
+// Release 1
 // Initialize a function takes two objects and checks to see if the objects share at least one key-value pair
-// var personOne = {name: "Steven", age: 54};
-// var personTwo = {name: "Tamir", age: 54};
 
 function valueCompare(personOne, personTwo) {
 	for (var i = 0; i < 2; i++) {
 		if (Object.values(personOne)[i] == Object.values(personTwo)[i]) {
+			return true;
 		}
-	return true;
 	}
 }
 
+// Release 2
 // Initialize a function that takes an integer for length, 
 // and builds and returns an array of strings of the given length.
-// var randNum = Math.floor(5)
 
 function wordBuilder(length) {
 	var wordArr = [];
 	var letters = "abcdefghijklmnopqrstuvwxyz";
 	var stringCreator = "";
-	for (var i = 0; i < length; i++) {
-	  	for (var x = 0; x < Math.floor(Math.random() * 10) + 1; x++) {
+	for (var i = 0; i < length; i++) { // Loop to determine how many words are in the array
+	  	for (var x = 0; x < Math.floor(Math.random() * 10) + 1; x++) { // loop to determine how many characters are in each word.
 	  		stringCreator += letters[Math.floor(Math.random() * letters.length)]; 
 	  	}
-  	wordArr.push(stringCreator);
-  	stringCreator = "";
+	  	wordArr.push(stringCreator);
+	  	stringCreator = "";
 	}
 	return wordArr;
 }
@@ -50,12 +48,17 @@ function wordBuilder(length) {
 
 // Driver Code
 
-stringDetector(["hi", "bye", "Hello world", "bri" ]);
-valueCompare({name: "Steven", age: 54}, {name: "Tamir", age: 54});
-wordBuilder(Math.floor(Math.random() * 10) + 1);
+// stringDetector(["hi", "bye", "Hello world", "bri" ]);
+// valueCompare({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+// wordBuilder(Math.floor(Math.random() * 10) + 1);
 
+// Driver code that generates an array, prints the array,
+// feeds the array to the stringDetector function, and prints the result 10 times.
 for (var i = 0; i < 10; i++) {
-
+	var genArray = wordBuilder(Math.floor(Math.random() * 10) + 1);
+	console.log(genArray);
+	console.log(stringDetector(genArray))
+}
 
 
 
