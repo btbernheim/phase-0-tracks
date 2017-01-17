c516d778fefa069ab34cc37c47b88cdee66fd760
Brian Bernheim <btbernheim@gmail.com>
2017-01-17 05:04:00 -0800

    Remove conditional statements for each commang.

--- databases/build_something/data_interface.rb
+++ databases/build_something/data_interface.rb
@@ -18,8 +18,8 @@ puts "Hello! Welcome to the User Information Storage App."
 puts "This app will hold all of your user and password information so you never have to deal with memorizing unique login information again!" 
 
 more_tries = ""
-answer = false 
-# until more_tries == "no"
+
+until more_tries == "no"
 	puts "Would you like to locate, store, update, or delete login information? (locate/store/update/delete)"
 	response = gets.chomp
 		if response == "store"
@@ -31,12 +31,10 @@ answer = false
 			password_answer = gets.chomp
 			user_info_db.execute("INSERT INTO info (website, user_name, password) VALUES (?, ?, ?)", [website_answer, user_name_answer, password_answer])
 			p user_info_db.execute("SELECT * FROM info")
-			answer == true
 			elsif response == "locate"
 				puts "What website do you need your user name and password for?"
 				locate_webiste_answer = gets.chomp
 				p user_info_db.execute("SELECT * FROM info WHERE website='#{locate_webiste_answer}'")
-				answer == true
 			elsif response == "update"
 				puts "Which websites login information would you like to update?"
 				update_login = gets.chomp
@@ -46,19 +44,16 @@ answer = false
 				new_password = gets.chomp
 				user_info_db.execute("UPDATE info SET user_name='#{new_user_name}', password='#{new_password}' WHERE website='#{update_login}'")
 				p user_info_db.execute("SELECT * FROM info WHERE website='#{update_login}'")
-				answer == true
 			elsif response == "delete"
 				puts "What website login information would you like to delete?"
 				delete_login = gets.chomp
 				user_info_db.execute("DELETE FROM info WHERE website='#{delete_login}'")
 				p user_info_db.execute("SELECT * FROM info")
-				answer == true
 			else puts "Please enter 'locate', 'store', 'update', or 'delete'."
-				answer == false
 		end
-# 	puts "Would like to look up or store more information?"
-# 	more_tries = gets.chomp
-# end
+ 	puts "Would like store, locate, update, or delete any other login information?"
+ 	more_tries = gets.chomp
+end
 
 
 # Test
