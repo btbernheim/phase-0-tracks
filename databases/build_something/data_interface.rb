e8f7156a88ab0b1561b4fac5fa0233ca57ca827d
Brian Bernheim <btbernheim@gmail.com>
2017-01-17 04:35:44 -0800

    Fix syntax for the variables inputed into the queries.

--- databases/build_something/data_interface.rb
+++ databases/build_something/data_interface.rb
@@ -15,48 +15,48 @@ SQL
 user_info_db.execute(create_info_table)
 
 puts "Hello! Welcome to the User Information Storage App."
-puts "This app will hold all of your user and password information so you never have to deal with memorizing unique passwords and users again!" 
+puts "This app will hold all of your user and password information so you never have to deal with memorizing unique login information again!" 
 
 more_tries = ""
 answer = false 
-until more_tries == "no"
-	puts "Would you like to locate, store, update, or delete login information? (locate/store)"
+# until more_tries == "no"
+	puts "Would you like to locate, store, update, or delete login information? (locate/store/update/delete)"
 	response = gets.chomp
-	until answer == true
-		if response = "store"
-			puts "What website or app do you wish to store information about?"
+	# until answer == true
+		if response == "store"
+			puts "What website do you wish to store information about?"
 			website_answer = gets.chomp
 			puts "Enter your username:"
 			user_name_answer = gets.chomp
 			puts "Enter your password:"
 			password_answer = gets.chomp
-			user_info_db.execute("INSERT INTO info (website, user_name, password) VALUES (#{website_answer}, #{user_name_answer}, #{password_answer})")
+			user_info_db.execute("INSERT INTO info (website, user_name, password) VALUES (?, ?, ?)", [website_answer, user_name_answer, password_answer])
 			answer == true
-			elsif response = "locate"
+			elsif response == "locate"
 				puts "What website do you need your user name and password for?"
 				locate_webiste_answer = gets.chomp
-				user_info_db.execute("SELECT * FROM info WHERE website=#{locate_webiste_answer}")
+				user_info_db.execute("SELECT * FROM info WHERE website='#{locate_webiste_answer}'")
 				answer == true
-			elsif response = "update"
+			elsif response == "update"
 				puts "Which websites login information would you like to update?"
 				update_login = gets.chomp
 				puts "Enter you new user name."
 				new_user_name = gets.chomp
 				puts "Enter your new password."
 				new_password = gets.chomp
-				user_info_db.execute("UPDATE info SET user_name=#{new_user_name}, password=#{new_password} WHERE website=#{update_login}")
+				user_info_db.execute("UPDATE info SET user_name='#{new_user_name}', password='#{new_password}'' WHERE website='#{update_login}'")
 				answer == true
-			elsif response = "delete"
+			elsif response == "delete"
 				puts "What is the wensite to login information you'd like to delete?"
 				delete_login = gets.chomp
 				answer == true
 			else puts "Please enter 'locate' or 'store'."
 				answer == false
 		end
-	end
-	puts "Would like to look up or store more information?"
-	more_tries = gets.chomp
-end
+	# end
+# 	puts "Would like to look up or store more information?"
+# 	more_tries = gets.chomp
+# end
 
 
 # Test
