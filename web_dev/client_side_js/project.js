4a5e4fd18c363b3bfacc5d3b8db9edff19ea10f3
Brian Bernheim <btbernheim@gmail.com>
2017-01-21 04:26:09 -0800

    Add code the chnages the font color of a p tag once the page has already loaded.

---
+++ web_dev/client_side_js/project.js
@@ -0,0 +1,20 @@
+var p_font_color = document.getElementById("question");
+
+
+
+p_font_color.style.color = "red";
+
+
+
+function p_on_click(event){
+	console.log("event done")
+	console.log(event);
+	p_answer.style.visibility = "visible";
+}
+
+answer.style.visibility = "hidden";
+
+var p_answer = document.getElementById("answer");
+
+
+click.addEventListener("click", p_on_click, p_answer);
